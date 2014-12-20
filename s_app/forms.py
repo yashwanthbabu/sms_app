9b2c7d8ce8272893f1b4cd81d937a58bac38fd4f
manjunath24 <hugar.manjunathrt@gmail.com>
2014-12-21 00:54:47 +0530

    text area for sms body

--- s_app/forms.py
+++ s_app/forms.py
@@ -1,11 +1,12 @@
 from django.forms import ModelForm
-from django.contrib.auth.models import User
+from django import forms
 
 from .models import Sms
 
 
 class SmsForm(ModelForm):
+    sms_text = forms.CharField(widget=forms.Textarea)
 
     class Meta:
         model = Sms
-        exclude = ['user', 'status',]
+        exclude = ['user', 'status', ]
