fbc4223177577913a6aa0cbf83d26cfd43b0ee17
manjunath24 <hugar.manjunathrt@gmail.com>
2014-12-20 19:47:36 +0530

    signin

--- Sms_App/settings.py
+++ Sms_App/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'parsley',
+
     's_app',
 )
 
@@ -81,3 +84,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+
+)
--- s_app/views.py
+++ s_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth import authenticate
+from django.contrib import messages
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                return redirect('home')
+            else:
+                messages.add_message(
+                    request, messages.INFO, 'Your account is invalid')
+                return render(request, 'Avant/HTML/extras-login.htm')
+        else:
+            messages.add_message(
+                request, messages.INFO, 'Invalid user credentials')
+            return render(request, 'Avant/HTML/extras-login.htm')
+    else:
+        return render(request, 'Avant/HTML/extras-login.htm')
